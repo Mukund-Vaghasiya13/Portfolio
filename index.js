47d386e048325677d80307cbf14b6e02350cf7f0
Mukund-Vaghasiya13 <mukundvaghasiya138@gmail.com>
2025-04-13 22:09:58 +0530

    Few chages

--- index.js
+++ index.js
@@ -16,6 +16,32 @@ const ProjectsModle = [
         discreption:
           "<b>ProgrammaticRecipes</b>: is iOS app which is build using UIKit ,it uses the programmatic approach to build the UI of the app. And This Project is inspired by the <b>Sean Allen's UIKit course.</b>",
       },
+      {
+        image:
+          "https://res.cloudinary.com/mukund13/image/upload/v1744561933/Portfolio/iw4dh057pnuiqzatx9a6.png",
+        link: "https://github.com/Mukund-Vaghasiya13/Newsly",
+        discreption:
+          "<b>Newsly</b>: is iOS app which is build using UIKit ,it uses the programmatic approach to build the UI of the app. And This Project is build using <b>Newsapi.org</b> Api",
+      },
+    ],
+  },
+  {
+    title: "SwiftUI",
+    project: [
+      {
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw1KR-fzeYzPxAII54p8RFZ7cxcpepRVVJrg&s",
+        link: "https://github.com/Mukund-Vaghasiya13/UIKit_GitHubApi",
+        discreption:
+          "<b>UIKit_GitHubApi</b>: is iOS app which is build using UIKit ,it uses GitHub Api to fetch the data of the user and repositories of the user. And This Project is inspired by the <b>Sean Allen's UIKit course.</b>",
+      },
+      {
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw1KR-fzeYzPxAII54p8RFZ7cxcpepRVVJrg&s",
+        link: "https://github.com/Mukund-Vaghasiya13/ProgrammaticRecipes",
+        discreption:
+          "<b>ProgrammaticRecipes</b>: is iOS app which is build using UIKit ,it uses the programmatic approach to build the UI of the app. And This Project is inspired by the <b>Sean Allen's UIKit course.</b>",
+      },
     ],
   },
 ];
